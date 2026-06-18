d1deb08b8f69f8b7c78b7ee6c68e2daef8f8229f
Light-moonx4 <125751899+Light-moonx4@users.noreply.github.com>
2026-06-18 10:47:16 -0500

    Add files via upload

---
+++ Script.sql
@@ -0,0 +1,12 @@
+create table authors(
+id serial primary key,
+name varchar(250)not null,
+numberphone char(10),
+email varchar(250)not null
+);
+create table users(
+id serial primary key,
+name varchar(250)not null,
+numberphone char(10),
+email vachar(250)not null
+)
